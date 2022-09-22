f6466d9f6e27d8a3964fdfe43f3d033672efe3be
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-09-22 10:00:49 -0500

    Llamado a módulo versionado en github

--- IaC-Terraform/practica-terraform1/app.tf
+++ IaC-Terraform/practica-terraform1/app.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 module "app-platzi" {
-  source = "./modulos/instance"
-  ami_id = var.ami_id
+  source        = "github.com/sebasvalencia726/Platzi/IaC-Terraform/practica-terraform1/modulos/instance"
+  ami_id        = var.ami_id
   instance_type = var.instance_type
-  tags = var.tags
-  sg_name = var.sg_name
+  tags          = var.tags
+  sg_name       = var.sg_name
   ingress_rules = var.ingress_rules
 }
