ef1eba31e7bd16e735967d898dd8beb6abef58be
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-10-23 17:24:39 -0500

    Paso de parámetros y opciones

---
+++ shell-programming/9-options.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Programa para ejemplificar como se realiza el paso de opciones sin parámetros
+# Autor: Sebastián Valencia
+
+echo "Programa Opciones"
+echo "Opción 1 enviada: $1"
+echo "Opción 2 enviada: $2"
+echo "Opciones enviadas: $*"
+echo -e "\n"
+echo "Recuperar valores"
+while [ -n "$1" ]
+do
+case "$1" in
+-a) echo "-a opción utilizada";;
+-b) echo "-b opción utilizada";;
+-c) echo "-c opción utilizada";;
+*)echo "$! no es una opción";;
+esac
+shift
+done
+
