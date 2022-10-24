0b914444fca3f3de7b7b064ee3d74d1c1bb80162
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-10-24 16:00:39 -0500

    If else statements

--- shell-programming/11-ifElse-2.sh
+++ shell-programming/11-ifElse-2.sh
@@ -2,21 +2,14 @@
 # Programa para ejemplificar el uso de la sentencia de decisión if, else. 
 # Autor: Sebastián Valencia
 
-notaClase=0
 edad=0
 
 echo "Ejemplo sentencia If - Else"
-read -n1 -p "Indique cúal es su nota (1-9):" notaClase
-echo -e "\n"
-if (( notaClase >= 7 )); then
-  echo "El alumno aprueba la materia"
-else
-  echo "El alumno reprueba la materia"
-fi
-
 read -p "Indique cúal es su edad:" edad
 if [ "$edad" -le 18 ]; then
-  echo "La persona no puede sufragar"
+  echo "La persona es adolecente"
+elif [ "$edad" -ge 19 ] && [ $edad -le 64 ]; then
+  echo "La persona es adulta"  
 else
-  echo "La persona puede sufragar"
+  echo "La persona es adulto mayor"
 fi
