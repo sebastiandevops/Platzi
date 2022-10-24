1810c365acf8166a3918dc3e855eed9f21dba278
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-10-24 10:19:41 -0500

    if else statement

---
+++ shell-programming/10-download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Programa para ejemplificar el uso de la descarga de información desde internet usando el comando wget.
+
+echo "Descargar información de iinternet"
+wget "$1"
---
+++ shell-programming/11-ifElse-2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Programa para ejemplificar el uso de la sentencia de decisión if, else. 
+# Autor: Sebastián Valencia
+
+notaClase=0
+edad=0
+
+echo "Ejemplo sentencia If - Else"
+read -n1 -p "Indique cúal es su nota (1-9):" notaClase
+echo -e "\n"
+if (( notaClase >= 7 )); then
+  echo "El alumno aprueba la materia"
+else
+  echo "El alumno reprueba la materia"
+fi
+
+read -p "Indique cúal es su edad:" edad
+if [ "$edad" -le 18 ]; then
+  echo "La persona no puede sufragar"
+else
+  echo "La persona puede sufragar"
+fi
