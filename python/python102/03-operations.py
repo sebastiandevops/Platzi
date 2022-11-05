9cb08e9c33760a2360af7605abb50458b4aebc33
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-11-05 07:21:40 -0500

    Set operations

---
+++ python/python102/03-operations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+set_a = {'col', 'mex', 'bol'}
+set_b = {'pe', 'bol'}
+
+set_c = set_a.union(set_b)
+print(set_c)
+print(set_a | set_b)
+
+set_c = set_a.intersection(set_b)
+print(set_c)
+print(set_a & set_b)
+
+set_c = set_a.difference(set_b)
+print(set_c)
+print(set_a - set_b)
