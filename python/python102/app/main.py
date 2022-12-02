34d3e1bab830c6941791cfde2fadbf8a2855fcf2
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-12-02 18:42:15 -0500

    Charts

--- python/python102/app/main.py
+++ python/python102/app/main.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 
 import utils
-
-data = [{'Country': 'Colombia',
-         'Population': 500},
-        {'Country': 'Bolivia',
-         'Population': 300}]
+import read_csv
+import charts
 
 
 def run():
-    keys, values = utils.get_population()
-    print(keys, values)
-
+    data = read_csv.read_csv('./data.csv')
     country = input('Type country => ')
-
     result = utils.population_by_country(data, country)
-    print(result)
 
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(labels, values)
 
 if __name__ == "__main__":
+
     run()
