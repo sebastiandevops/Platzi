9df3e1c80be05b65743f5b1104d5cb645a40b737
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-12-02 18:01:36 -0500

    Data read and visualization

---
+++ python/python102/app/read-csv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import csv
+
+
+def read_csv(path):
+    with open(path, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        header = next(reader)
+        data = []
+        for row in reader:
+            iterable = zip(header, row)
+            country_dict = {key: value for key, value in iterable}
+            data.append(country_dict)
+        return data
+
+
+if __name__ == '__main__':
+    data = read_csv('./data.csv')
+    print(data[0])
