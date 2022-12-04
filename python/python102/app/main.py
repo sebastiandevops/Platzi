2d49bc94a607f74dd9940cce389f447b5d1c0a00
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2022-12-04 13:52:49 -0500

    More graphs

--- python/python102/app/main.py
+++ python/python102/app/main.py
@@ -7,14 +7,20 @@ import charts
 
 def run():
     data = read_csv.read_csv('./data.csv')
-    country = input('Type country => ')
-    result = utils.population_by_country(data, country)
+    data = list(filter(lambda item: item['Continent'] == 'South America',
+                       data))
 
-    if len(result) > 0:
-        country = result[0]
-        labels, values = utils.get_population(country)
-        charts.generate_bar_chart(labels, values)
+    countries = list(map(lambda x: x['Country/Territory'], data))
+    percentages = list(map(lambda x: x['World Population Percentage'], data))
+    charts.generate_pie_chart(countries, percentages)
+    # country = input('Type country => ')
+    # result = utils.population_by_country(data, country)
+
+    # if len(result) > 0:
+    #     country = result[0]
+    #     labels, values = utils.get_population(country)
+    #     charts.generate_bar_chart(labels, values)
+
 
 if __name__ == "__main__":
-
     run()
