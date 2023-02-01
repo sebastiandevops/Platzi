1b756de00961e1cfe95022da9eabcf1bb22ddbe2
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-01 10:17:34 -0500

    First python class object

--- object-oriented-programming/Python/main.py
+++ object-oriented-programming/Python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hola, Mundo")
+    car = Car()
+    car.license = "AMS234"
+    car.drive = "Andrés Herrera"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Manuel Herrera"
+    print(vars(car2))
