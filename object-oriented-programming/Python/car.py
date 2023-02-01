c1f713e127b8fe850aefbe3bbf59ef95476d6b84
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-01 11:44:53 -0500

    Python constructor

--- object-oriented-programming/Python/car.py
+++ object-oriented-programming/Python/car.py
@@ -1,5 +1,12 @@
+from account import Account
+
+
 class Car:
     id = int
     license = str
-    driver = str
-    passenger = str
+    driver = Account("", "")
+    passenger = int
+
+    def __init__(self, license, driver):
+        self.license = license
+        self.driver = driver
--- object-oriented-programming/Python/main.py
+++ object-oriented-programming/Python/main.py
@@ -1,13 +1,8 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hola, Mundo")
-    car = Car()
-    car.license = "AMS234"
-    car.drive = "Andrés Herrera"
+    car = Car("AMS234", Account("Andrés Herrera", "ANDA876"))
     print(vars(car))
-
-    car2 = Car()
-    car2.license = "QWE567"
-    car2.driver = "Manuel Herrera"
-    print(vars(car2))
+    print(vars(car.driver))
