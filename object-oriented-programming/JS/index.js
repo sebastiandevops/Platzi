9d0b015e967e9e99b373f6200c57b6c50ec7f115
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-01 16:20:56 -0500

    JS subclasses

--- object-oriented-programming/JS/index.js
+++ object-oriented-programming/JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andrés Herrera", "AMD12"))
 car.passenger = 4
 car.printDataCar();
+
+var uberX = new UberX("AW678", new Account("Andrea Ferrán", "AND678"), "Chevron", "SSparks")
+uberX.passenger = 4;
+uberX.printDataCar();
