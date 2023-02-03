f6c98efe21a2e0c2a79576c559df2bcf1bdd6450
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-03 09:48:11 -0500

    Turtle module for Python

---
+++ python/turtle_square.py
@@ -0,0 +1,18 @@
+import turtle
+
+window = turtle.Screen()
+myTurtle = turtle.Turtle()
+
+myTurtle.forward(100)
+myTurtle.right(90)
+
+myTurtle.forward(100)
+myTurtle.right(90)
+
+myTurtle.forward(100)
+myTurtle.right(90)
+
+myTurtle.forward(100)
+myTurtle.right(90)
+
+window.mainloop()
