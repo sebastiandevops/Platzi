6737ffcba01e0455de277c63a868abebb8395f8b
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-04 11:27:03 -0500

    create_clients and list_clients functions

--- python/crud-python/main.py
+++ python/crud-python/main.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python3
 
-clients = 'Pablo, Ricardo'
+clients = 'Pablo,Ricardo'
+
+
+def create_client(client_name):
+    """Function to create clients
+
+    Args:
+        client_name (): string name of the client to create
+    """
+    global clients
+    clients += ',' + client_name
+
+
+def list_clients():
+    """ Function to list clients """
+    global clients
+    print(clients)
 
 
 if __name__ == '__main__':
-    clients += ', David'
-    print(clients)
+    list_clients()
+    create_client("David")
+    list_clients()
