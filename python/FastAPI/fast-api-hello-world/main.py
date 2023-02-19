9acc1448c0272f66a8d382119196606ea731f259
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-19 18:07:24 -0500

    request_response_body

--- python/FastAPI/fast-api-hello-world/main.py
+++ python/FastAPI/fast-api-hello-world/main.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python3
 
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
+
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 
 @app.get("/")
 def home():
     return {"Hello": "World"}
+
+
+# Request and Response Body
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
