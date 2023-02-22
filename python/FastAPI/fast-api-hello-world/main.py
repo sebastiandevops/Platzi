4bd9c62efd46940b0b7c5671e790d8f870e8e2a0
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-22 07:06:32 -0500

    files

--- python/FastAPI/fast-api-hello-world/main.py
+++ python/FastAPI/fast-api-hello-world/main.py
@@ -13,7 +13,7 @@ from pydantic import EmailStr
 # FastAPI
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 app = FastAPI()
 
@@ -226,3 +226,17 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+
+# Files
+@app.post(
+    path="/post-image"
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+            "Filename": image.filename,
+            "Format": image.content_type,
+            "Size(kb)": round(len(image.file.read()) / 1024, ndigits=2)
+    }
