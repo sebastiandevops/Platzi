6ba1eca448507ab6d61b6d84db6cefd205fde55c
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-25 16:07:53 -0500

    users_signup_docstring

--- python/FastAPI/twitter-api-fastapi/main.py
+++ python/FastAPI/twitter-api-fastapi/main.py
@@ -45,6 +45,14 @@ class User(UserBase):
     birth_date: Optional[date] = Field(default=None)
 
 
+class UserRegister(User):
+    password: str = Field(
+        ...,
+        min_length=8,
+        max_length=64
+    )
+
+
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
     content: str = Field(
@@ -70,6 +78,23 @@ class Tweet(BaseModel):
     tags=["Users"]
 )
 def signup():
+    """
+    Signup
+
+    This path operation registers a user in the app
+
+    Parameters:
+        - Request body parameter.
+            - user: UserRegister
+
+    Returns a json with the basic user information.
+        - user_id: UUID
+        - email: EmailStr
+        - first_name: str
+        - last_name: str
+        - birth_date: str
+        -
+    """
     pass
 
 
