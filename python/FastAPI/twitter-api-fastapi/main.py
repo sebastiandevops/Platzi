61bf3a5037f084b148a6f4721b28dbd14718a441
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-25 12:56:48 -0500

    models_users

--- python/FastAPI/twitter-api-fastapi/main.py
+++ python/FastAPI/twitter-api-fastapi/main.py
@@ -2,9 +2,13 @@
 
 # Python
 from uuid import UUID
+from datetime import date
+from typing import Optional
 
 # Pydantic
 from pydantic import BaseModel
+from pydantic import EmailStr
+from pydantic import Field
 
 # FastAPI
 from fastapi import FastAPI
@@ -13,11 +17,33 @@ app = FastAPI()
 
 
 # Models
+class UserBase(BaseModel):
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(...)
+
+
+class UserLogin(UserBase):
+    password: str = Field(
+        ...,
+        min_length=8
+    )
+
+
 class User():
-    user_id = UUID
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+    )
+    birth_date: Optional[date] = Field(default=None)
 
 
-class Tweet():
+class Tweet(BaseModel):
     pass
 
 
