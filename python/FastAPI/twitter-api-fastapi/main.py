a5079fe32f0ccb76eb0d22a8a9061bf3a3bc9ea6
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-25 17:52:19 -0500

    home

--- python/FastAPI/twitter-api-fastapi/main.py
+++ python/FastAPI/twitter-api-fastapi/main.py
@@ -193,7 +193,22 @@ def update_a_user():
     tags=["Tweets"]
 )
 def home():
-    return {"TwitterAPI": "Working!"}
+    """
+    This path operation shows al tweets in the app
+
+    Parameters:
+        -
+
+    Returns a json list with all tweets in the app, with the following keys:
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+    with open("tweets.json", "r", encoding="utf8") as f:
+        results = json.loads(f.read())
+        return results
 
 
 # Post a Tweet
