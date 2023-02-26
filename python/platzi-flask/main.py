0507453a208b0f71ff1a01d822033865c0425b11
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-26 16:45:49 -0500

    request_and_response

--- python/platzi-flask/main.py
+++ python/platzi-flask/main.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     user_ip = request.remote_addr
+    response = make_response(redirect("/hello"))
+    response.set_cookie("user_ip", user_ip)
+    return response
+
+
+@app.route("/hello")
+def hello():
+    user_ip = request.cookies.get("user_ip")
     return "Hello, Flask! Your ip is {}".format(user_ip)
 
 
