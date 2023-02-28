05a444209ad1b80bad36b7e82aa9dde5c5223b82
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-27 20:50:46 -0500

    errorhandler and static files

--- python/platzi-flask/main.py
+++ python/platzi-flask/main.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 todos = ["Buy coffee", "Develop a website", "Publish a project"]
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("404.html", error=error)
+
+
 @app.route("/")
 def index():
     user_ip = request.remote_addr
