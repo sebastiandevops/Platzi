864b527ba7b8f233cf89f830fa2eda2699fe2ab8
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-28 17:55:06 -0500

    flask config: session

--- python/platzi-flask/main.py
+++ python/platzi-flask/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, make_response, redirect, render_template
+from flask import session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config["SECRET_KEY"] = "SUPER SECRETO"
 
 todos = ["Buy coffee", "Develop a website", "Publish a project"]
 
@@ -23,13 +26,13 @@ def server_error(error):
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect("/hello"))
-    response.set_cookie("user_ip", user_ip)
+    session["user_ip"] = user_ip
     return response
 
 
 @app.route("/hello")
 def hello():
-    user_ip = request.cookies.get("user_ip")
+    user_ip = session.get("user_ip")
     context = {
         "user_ip": user_ip,
         "todos": todos,
