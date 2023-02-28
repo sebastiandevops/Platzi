f77bd87a4d5807b4e1581d6e595fc713d38969ba
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-02-28 18:55:06 -0500

    post method in flask_wtf

--- python/platzi-flask/main.py
+++ python/platzi-flask/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, make_response, redirect, render_template
-from flask import session
+from flask import session, url_for
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
@@ -39,15 +39,22 @@ def index():
     return response
 
 
-@app.route("/hello")
+@app.route("/hello", methods=["GET", "POST"])
 def hello():
     user_ip = session.get("user_ip")
     login_form = LoginForm()
+    username = session.get("username")
     context = {
         "user_ip": user_ip,
         "todos": todos,
         "login_form": login_form,
+        "username": username,
     }
+    # if login_form.validate_on_submit():
+    if request.method == 'POST':
+        username = login_form.username.data
+        session["username"] = username
+        return redirect(url_for("index"))
     return render_template("hello.html", **context)
 
 
