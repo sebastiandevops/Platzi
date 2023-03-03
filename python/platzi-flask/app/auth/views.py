03f3662c331a3a7b9e6215a6aae939dd4210dd6a
Sebastian Valencia Sierra <sebasvalencia726@gmail.com>
2023-03-03 15:01:35 -0500

    Signup

--- python/platzi-flask/app/auth/views.py
+++ python/platzi-flask/app/auth/views.py
@@ -4,8 +4,9 @@ from flask import render_template, session, redirect, flash, url_for
 from flask_login import login_user, login_required, logout_user
 from app.forms import LoginForm
 from . import auth
-from app.firestore_service import get_user
+from app.firestore_service import get_user, user_put
 from app.models import UserModel, UserData
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 @auth.route("/login", methods=["GET", "POST"])
@@ -22,7 +23,10 @@ def login():
 
         if user_doc.to_dict() is not None:
             password_from_db = user_doc.to_dict()["password"]
-            if password == password_from_db:
+            # if password == password_from_db:
+            #     user_data = UserData(username, password)
+            #     user = UserModel(user_data)
+            if check_password_hash(password_from_db, password):
                 user_data = UserData(username, password)
                 user = UserModel(user_data)
 
@@ -36,6 +40,35 @@ def login():
 
         return redirect(url_for("index"))
     return render_template("login.html", **context)
+
+
+@auth.route("signup", methods=["GET", "POST"])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        "signup_form": signup_form
+    }
+
+    if signup_form.validate_on_submit():
+        username = signup_form.username.data
+        password = signup_form.password.data
+
+        user_doc = get_user(username)
+
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+
+            user = UserModel(user_data)
+            login_user(user)
+            flash("Welcome!")
+            return redirect(url_for("hello"))
+
+        else:
+            flash("User already exists.")
+
+    return render_template("signup.html", **context)
 
 
 @auth.route("logout")
--- python/platzi-flask/app/firestore_service.py
+++ python/platzi-flask/app/firestore_service.py
@@ -22,3 +22,8 @@ def get_todos(user_id):
     return db.collection('users')\
             .document(user_id)\
             .collection('todos').get()
+
+
+def user_put(user_data):
+    user_ref = db.collection("users").document(user_data.username)
+    user_ref.set({"password": user_data.password})
